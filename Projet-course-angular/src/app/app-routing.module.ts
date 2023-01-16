905c228be0ca1f27a0d641583dc930a9594bcfaf
Thomas <ourliac.du.82@gmail.com>
2023-01-16 15:28:56 +0100

    import résolu

--- Projet-course-angular/src/app/app-routing.module.ts
+++ Projet-course-angular/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './connexion/connexion.component';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: "", component: ConnexionComponent, pathMatch :'full'},
-  {path: "home", component: HomeComponent}
 
 ];
 

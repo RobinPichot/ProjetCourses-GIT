24866ff81d00c065a07bb67938f2919608f48d9e
Thomas <ourliac.du.82@gmail.com>
2023-01-19 15:57:01 +0100

    resolution temps recup

--- Projet-course-angular/src/app/client-page-recherche/client-page-recherche.component.ts
+++ Projet-course-angular/src/app/client-page-recherche/client-page-recherche.component.ts
@@ -1,58 +1,56 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { faThumbsDown } from '@fortawesome/free-solid-svg-icons';
-import { Commande } from '../model';
-import { VariableCompteConnecte } from '../VariableGlobale';
-import { ClientPageRechercheHttpService } from './client-page-recherche-http.service';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { faThumbsDown } from "@fortawesome/free-solid-svg-icons";
+import { Commande } from "../model";
+import { VariableCompteConnecte } from "../VariableGlobale";
+import { ClientPageRechercheHttpService } from "./client-page-recherche-http.service";
 
 @Component({
-  selector: 'app-client-page-recherche',
-  templateUrl: './client-page-recherche.component.html',
-  styleUrls: ['./client-page-recherche.component.scss']
+  selector: "app-client-page-recherche",
+  templateUrl: "./client-page-recherche.component.html",
+  styleUrls: ["./client-page-recherche.component.scss"],
 })
 export class ClientPageRechercheComponent {
-
-  ville:string;
-commande: Array<Commande>;
-last:Commande;
-  constructor(       private router: Router,
+  ville: string;
+  commande: Array<Commande>;
+  last: Commande;
+  constructor(
+    private router: Router,
     private variableGlobal: VariableCompteConnecte,
-    private clientService : ClientPageRechercheHttpService
-    ){}
-    
-    recherche(){
+    private clientService: ClientPageRechercheHttpService
+  ) {}
 
-      this.variableGlobal.villeRecherche=this.ville;
-      this.router.navigate(['/clientRestaurantRecherche']);
-      this.clientService.create(this.variableGlobal.idConnecte);
-      
-      this.clientService.findCommandeById(this.variableGlobal.idConnecte).subscribe(
-        commande =>{
-        this.commande=commande
-        this.last = [...this.commande].pop()
-        this.variableGlobal.idCommandeEnCour=this.last.id
-    })
-    
+  recherche() {
+    this.variableGlobal.villeRecherche = this.ville;
+    this.router.navigate(["/clientRestaurantRecherche"]);
+    this.clientService.create(this.variableGlobal.idConnecte).subscribe(() => {
+      this.clientService
+        .findCommandeById(this.variableGlobal.idConnecte)
+        .subscribe((commande) => {
+          this.commande = commande;
+          this.last = [...this.commande].pop();
+          this.variableGlobal.idCommandeEnCour = this.last.id;
+          console.log(this.variableGlobal.idCommandeEnCour);
+        });
+    });
   }
 
-    monCompte(){
-      //chemin vers mon compte
-      console.log("MON COMPTE !")
-      this.router.navigate(['/client/']); //POUR MON COMPTE DU CLIENT CO (CHANGER REQUETE DANS COMPTE UTILISATEUR APRES)
-      
-    }
+  monCompte() {
+    //chemin vers mon compte
+    console.log("MON COMPTE !");
+    this.router.navigate(["/client/"]); //POUR MON COMPTE DU CLIENT CO (CHANGER REQUETE DANS COMPTE UTILISATEUR APRES)
+  }
 
-    monPanier(){
-      //chemin vers mon panier
-      this.router.navigate(['/panier']);
-      console.log("MON PANIER !")
+  monPanier() {
+    //chemin vers mon panier
+    this.router.navigate(["/panier"]);
+    console.log("MON PANIER !");
+  }
 
-    }
-
-    logOut(){
-      this.variableGlobal.idConnecte=null;
-      this.variableGlobal.loginConnecte=null;
-      this.variableGlobal.villeRecherche=null;
-      this.router.navigate(['']);
-    }
+  logOut() {
+    this.variableGlobal.idConnecte = null;
+    this.variableGlobal.loginConnecte = null;
+    this.variableGlobal.villeRecherche = null;
+    this.router.navigate([""]);
+  }
 }
--- Projet-course-angular/src/app/client-restaurant-recherche/page-restaurant-recherche-http.service.ts
+++ Projet-course-angular/src/app/client-restaurant-recherche/page-restaurant-recherche-http.service.ts
@@ -2,15 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { Restaurant } from '../model';
+import { AppConfigService } from '../app-config.service';
+import { Commande, Restaurant } from '../model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PageRestaurantRechercheHttpService {
+  serviceUrl : string;
+  commande : Array<Commande>= new Array<Commande>();
 
 
-  constructor(private http: HttpClient,private router: Router){}
+  constructor(private http: HttpClient,private router: Router,private appConfig: AppConfigService){}
 
   
   charge(ville:string): Observable<Array<Restaurant>>{
@@ -18,5 +21,11 @@ export class PageRestaurantRechercheHttpService {
   }
 
  
+  private load(): void {
+    this.http.get<Array<Commande>>(this.serviceUrl).subscribe(response => {
+      this.commande = response;
+    });
+}
+
   
 }
